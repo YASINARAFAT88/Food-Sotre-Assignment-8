597d7263b436873fdc856438e39e5f56ecd37f08
HP 840 <yasin80545859@.com>
2022-03-22 16:56:35 +0600

    Try to git push

---
+++ src/component/Menuber/Menuber.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Menuber.css'
+
+const Menuber = (props) => {
+    return (
+        <nav>
+        <div className="container">
+            <div className="row">
+            <div className="col-md-2 p-2">logo</div>
+            <div className="col-md-10 menu-container d-flex justify-content-end">
+                <li className='items me-4 p-2'>Home</li>
+                <li className='items me-4 p-2'>Contact</li>
+                <li className='items me-4 p-2'>Cart <sup>{props.count}</sup> </li>
+                <li className='items me-4 p-2'>Log in</li>
+            </div>
+            </div>
+        </div>
+        </nav>
+    );
+};
+
+export default Menuber;
