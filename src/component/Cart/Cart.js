6bb84a1c362d3dbf8916eb3d7e3ec172a1444b16
HP 840 <yasin80545859@.com>
2022-03-24 14:55:33 +0600

    All Most Complate

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Cart.css'
+import logo from '../../images/Logo.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDotCircle, faDrum, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Cart = (props) => {
     const { cart } = props;
@@ -10,16 +13,22 @@ const Cart = (props) => {
         shipping = shipping + product.shipping
     }
     const tax = parseFloat((total * 0.1).toFixed(2))
+    const grandTotal = total + shipping + tax;
     console.log(cart)
     return (
         <div className='cart'>
+            <img className='logo' src={logo} alt="" />
             <h2>Oreder Summary</h2>
             <h4>Selected item {cart.length}</h4>
             <h4>Total price: ${total}</h4>
             <h4>Total Shipping ${shipping}</h4>
             <h4>Tax ${tax}</h4>
-            <h3>Grand Total: </h3>
+            <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
+            <button className='place-order'>Place Order <FontAwesomeIcon icon={faShoppingCart} /></button>
+             <br />
+            <button className='delate-items'>Delate Items <FontAwesomeIcon icon={faDotCircle} /></button>
         </div>
+        
     );
 };
 
