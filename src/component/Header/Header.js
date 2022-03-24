87a7a7a7ce1d91abb24ae19d92700c65eb43128c
HP 840 <yasin80545859@.com>
2022-03-24 09:48:38 +0600

    first commit

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import logo from '../../images/Logo.svg'
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div className="">
+                <input type="text" />
+                <button>Search</button>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Order</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
     );
 };
 
